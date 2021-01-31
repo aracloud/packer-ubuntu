3630150c8c84a7336239fc7c37a6273683ee6b07
aracloud <aracloud@gmx.net>
2021-01-31 17:35:35 +0100

    update host

--- script.sh
+++ script.sh
@@ -1,16 +1,10 @@
 #!/bin/bash -eux
 
-# Install Ansible repository.
+# Install  tools needed
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install \
 software-properties-common \
 ansible \
 net-tools
-
-# sudo apt-add-repository ppa:ansible/ansible -y
-
-# Install Ansible.
-#sudo apt -y update
-#sudo apt -y install ansible
 
